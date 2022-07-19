0eb6cf3b550b2e3b2ff525df77c0a78cebae9119
Megha <choudharymegha.02@gmail.com>
2022-07-19 09:16:42 +0530

    assignment 1

---
+++ main1.py
@@ -0,0 +1,39 @@
+"""my_string=input("Enter string:")
+if(my_string==my_string[::-1]):
+   print("The string is a palindrome")
+else:
+   print("The string isn't a palindrome")"""
+
+"""list_num=[1,2,3]
+tup_num=(2,3,5)
+print(list_num)
+print(tup_num)"""
+
+"""list_1=[20,10,40,30]
+print(list_1)
+tune=(list_1)
+print(tune)"""
+
+"""tune_a=(1,2,3,4)
+print(tune_a)
+tune_b=list(tune_a)
+#tune_b.append(20)
+tune_b.remove(3)
+tune_c=tuple(tune_b)
+print(tune_c)"""
+
+"""str=('m','e','g','h','a')
+str1=''.join(str)
+print(str)"""
+
+"""tune=('a','p','r','e','e','l')
+print(tune)
+tune_1=tune[0:3]
+print(tune_1)
+tune_1=tune[-4:-1]
+print(tune_1)"""
+
+d2 =["megha":"pizza","rapti":"burger","witch":"noodles"]
+print(d2)
+
+
